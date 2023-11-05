200983e6cce54655f5688832ce159db5a22d6ff4
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-05 16:30:02 +0100

    adding background image to linear-gradient

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
+import  BackGroundimage  from "./assets/images/background.png"
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={BackGroundimage}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backGroundimage}
+        >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
 
 
@@ -14,6 +22,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   rootScreen: {
-    flex: 1, 
+    flex: 1,
+  },
+  backGroundimage:{
+    opacity:0.15
   }
 });
