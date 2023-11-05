a37d0e3c9d4fb83f501dab80cf91a660ae6a4cce
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-05 21:39:57 +0100

    random number generation

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
-import { Text, View,StyleSheet } from 'react-native'
-import Title from '../components/Title'
+import { Text, View, StyleSheet } from 'react-native'
+import Title from '../components/ui/Title'
+import { useState } from 'react';
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+
+  const initialGuess = generateRandomBetween(1, 100, userNumber)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
   return (
     <View style={styles.screen}>
-       <Title >Opponent's Guess</Title>
-       {/***GUESS**/}
-        <View>
-            <Text>Higher or lowe?</Text>
-       {/***+-**/}
-        </View>
-        <View><Text>Log Rounds</Text></View>
+      <Title >Opponent's Guess</Title>
+      <Text> </Text>
+      <View>
+        <Text>Higher or lowe?</Text>
+        {/***+-**/}
+      </View>
+      <View><Text>Log Rounds</Text></View>
     </View>
   )
 }
 
 export default GameScreen
 
-const styles=StyleSheet.create({
-    screen:{
-        flex:1,
-        padding:22,
-    },
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 22,
+  },
 
 })
