085d4d2873dd103ba7a784481d84e831b045e87b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-05 21:38:49 +0100

    rearrangement of components for transparency

--- components/ui/PrimaryButton.jsx
+++ components/ui/PrimaryButton.jsx
@@ -1,13 +1,13 @@
 
 import { Text, View, Pressable, StyleSheet } from 'react-native'
-import Colors from '../constans/colors'
+import Colors from '../../constans/colors'
 
 
-function PrimaryButton({ children,onPress }) {
+function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterConteiner}>
       <Pressable
-        style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}       
+        style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
         android_ripple={{ color: Colors.primary600 }}
         onPress={onPress}>
         <Text style={styles.buttonText}>
@@ -25,14 +25,14 @@ const styles = StyleSheet.create({
     borderRadius: 28,
     margin: 4,
     overflow: 'hidden',
-   
+
   },
   buttonInnerContainer: {
-    backgroundColor: Colors.primary500,    
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
-    
+
   },
   buttonText: {
     color: 'white',
---
+++ components/ui/Title.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import Colors from '../../constans/colors'
+
+function Title({ children }) {
+    return (
+        <View>
+            <Text style={styles.title}> {children}</Text>
+        </View>
+    )
+}
+
+export default Title
+
+const styles = StyleSheet.create({
+
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: Colors.accent500,
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: Colors.accent500,
+        padding: 12,
+        marginVertical: 30,
+    }
+})
