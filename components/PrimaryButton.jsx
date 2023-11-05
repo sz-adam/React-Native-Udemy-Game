69895738fc405f270e5e74db7769f65a83c5bd76
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-05 11:41:00 +0100

    StartGameScreen input button +style

---
+++ components/PrimaryButton.jsx
@@ -0,0 +1,14 @@
+
+import { Text, View } from 'react-native'
+
+function PrimaryButton({children}) {
+  return (
+    <View>
+        <Text>
+            {children}
+        </Text>
+    </View>
+  )
+}
+
+export default PrimaryButton
---
+++ screens/StartGameScreen.jsx
@@ -0,0 +1,41 @@
+import { TextInput, View, StyleSheet } from 'react-native'
+import PrimaryButton from '../components/PrimaryButton'
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2}/>
+      <PrimaryButton >Reset</PrimaryButton>
+      <PrimaryButton >Confrirm</PrimaryButton>
+    </View>
+
+  )
+}
+
+export default StartGameScreen
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4, //androidon shadow
+    shadowColor: 'black', //ios shadow
+    shadowOffset: { width: 0, height: 2 },//ios shadow
+    shadowRadius: 6,//ios shadow
+    shadowOpacity: 0.25,//ios shadow
+  },
+  numberInput: {
+    height: 50,
+    width:50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth:2,
+    color:'#ddb52f',
+    marginVertical:8,
+    fontWeight:'bold',
+    textAlign:'center'
+  },
+})
