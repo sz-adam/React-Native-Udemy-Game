6bf445b839ee742f353a3cc697ebb04b9d99b69a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-11-12 15:39:27 +0100

    add dimensions api /responsive

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Dimensions } from 'react-native'
 import Colors from '../../constans/colors'
 
 function Card({ children }) {
@@ -12,12 +12,14 @@ function Card({ children }) {
 
 export default Card
 
+const deviceWidth = Dimensions.get('window').width
+
 const styles = StyleSheet.create({
     card: {
         justifyContent: 'center',
         alignItems: 'center',
         padding: 16,
-        marginTop: 36,
+        marginTop: deviceWidth < 380 ? 18 : 36,
         backgroundColor: Colors.primary800,
         marginHorizontal: 24,
         borderRadius: 8,
